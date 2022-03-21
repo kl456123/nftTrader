81be35e0a91f3d45b1647985ed0c87620f6bfc32
liang xiong <liangxiong@okg.com>
2022-03-21 16:32:08 +0800

    upload contracts

---
+++ contracts/MockNFT.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol';
+
+contract MockNFT is ERC721PresetMinterPauserAutoId {
+    constructor() public ERC721PresetMinterPauserAutoId('Mock NFT', 'MNFT', 'https://mocknft.com') {}
+}
---
+++ contracts/MockToken.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol';
+
+contract MockToken is ERC20PresetMinterPauser {
+    constructor() public payable ERC20PresetMinterPauser('MockToken', 'MT') {}
+}
